61fc69395402e23f7f6fe251730b9bc470cc2847
JAmoMES <atipat.pa@ku.th>
2021-02-13 15:30:09 +0700

    add GET POST

--- test1.py
+++ test1.py
@@ -2,29 +2,57 @@ from flask import Flask, request
 from flask_pymongo import PyMongo
 import time
 import math
-i = 0
 
 app = Flask(__name__)
 app.config['MONGO_URI'] = 'mongodb://exceed_user:1q2w3e4r@158.108.182.0:2277/exceed_backend'
 mongo = PyMongo(app)
-
 myCollection = mongo.db.g16
+i = 0
+cost_t = [0]
 
 @app.route('/status', methods=['GET'])
 def find():
+    slot = request.args.get("slot")
+    flit = {"slot_number": slot ,"time_out": None}
+    query = myCollection.find(flit)
+    output = [{"status": 0}]
+    for ele in query:
+        output[ele["status"]] = 1
+    return { "result": output }
+
+@app.route('/cost', methods=['GET'])
+def cost():
+    flit = request.args
+    query = myCollection.find(flit)
+    money = int(math.ceil((query["time_out"] - query["time_in"])/60)*20)
+    output = [{cost: money}]
+    return { "result": output}
+
+@app.route('/time_cost', methods=['GET'])
+def array_cost():
     flit = {"time_out": None}
     query = myCollection.find(flit)
-    output = [{0:0},{1:0},{2:0},{3:0}]
+    global cost_t
+    summ = cost_t[-1]
+    t = int(math.ceil(time.time()))
     for ele in query:
-        output[ele["slot_number"]] = 1
-    return { "result": output }
+        sum += int(math.ceil((t - ele["time_in"])/60)*20)
+    cost_t.append(sum)
+    return { "result": cost_t}
 
 @app.route('/updatecar', methods=['POST'])
 def insert_one():
     data = request.json
     if data["Status"] == 1:
+        global i
+        i +=1 
+        flit = {"slot_number": data["No"] ,"time_out": None}
+        query = myCollection.find(flit)
+        if(len(query)!=0):
+            return {'result': 'slot full'}
         myInsert = {
                 "type": "car",
+                "ID" : i,
                 "slot_number" : data["No"],
                 "time_in": int(math.ceil(time.time())),
                 "time_out": None
@@ -32,8 +60,8 @@ def insert_one():
         myCollection.insert_one(myInsert)
         return {'result': 'add car in'}
     else :
-        filt = {{'slot_number' : data["No"]},
-                {"time_out": None}
+        filt = {"slot_number" : data["No"],
+                "time_out": None
                 }
         updated_content = {"$set": {'time_out' : int(math.ceil(time.time()))}}
         myCollection.update_one(filt, updated_content)
