379f00b4f088710e2f8e2b3895b1eef1a97d8936
JAmoMES <atipat.pa@ku.th>
2021-02-13 13:18:58 +0700

    add GET POST

--- test1.py
+++ test1.py
@@ -10,18 +10,38 @@ myCollection = mongo.db.g16
 
 @app.route('')
 
-@app.route('/create', methods=['POST'])
+@app.route('/store', methods=['GET'])
+def find():
+    flit = {"time_out": None}
+    query = myCollection.find(flit)
+    output = [{0:0},{1:0},{2:0},{3,0}]
+    for ele in query:
+        output[ele["slot_number"]] = 1
+    return { "result": output }
+
+@app.route('/addcar', methods=['POST'])
 def insert_one():
     data = request.json
     myInsert = {
-            "car_number": data["car_number"],
-            ""
-            "time_in": data["time_in"],
-            "time_out": data["time_out"]
+            "type": "car",
+            "slot_number" : data["No"],
+            "time_in": time.time(),
+            "time_out": None
             }
     myCollection.insert_one(myInsert)
     return {'result': 'Created successfully'}
 
+@app.route('/addcar', methods=[''])
+def insert_one():
+    data = request.json
+    myInsert = {
+            "type": "car",
+            "slot_number" : data["No"],
+            "time_in": time.time(),
+            "time_out": None
+            }
+    myCollection.insert_one(myInsert)
+    return {'result': 'Created successfully'}
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='50005', debug=True)
     
