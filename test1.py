fbc3c6ac034bd9d4b98d52c939574a7c1b743c5e
JAmoMES <atipat.pa@ku.th>
2021-02-13 14:27:59 +0700

    add GET POST

--- test1.py
+++ test1.py
@@ -1,14 +1,14 @@
 from flask import Flask, request
 from flask_pymongo import PyMongo
 import time
+import math
+i = 0
 
 app = Flask(__name__)
 app.config['MONGO_URI'] = 'mongodb://exceed_user:1q2w3e4r@158.108.182.0:2277/exceed_backend'
 mongo = PyMongo(app)
 
 myCollection = mongo.db.g16
-
-@app.route('')
 
 @app.route('/status', methods=['GET'])
 def find():
@@ -22,17 +22,20 @@ def find():
 @app.route('/updatecar', methods=['POST'])
 def insert_one():
     data = request.json
-    if data["status"] == 1:
+    if data["Status"] == 1:
         myInsert = {
                 "type": "car",
                 "slot_number" : data["No"],
-                "time_in": time.time(),
+                "time_in": int(math.ceil(time.time())),
                 "time_out": None
                 }
+        myCollection.insert_one(myInsert)
         return {'result': 'add car in'}
     else :
-        filt = {{'slot_number' : data["No"]},{"time_out": None}}
-        updated_content = {"$set": {'time_out' : time.time()}}
+        filt = {{'slot_number' : data["No"]},
+                {"time_out": None}
+                }
+        updated_content = {"$set": {'time_out' : int(math.ceil(time.time()))}}
         myCollection.update_one(filt, updated_content)
         return {'result': 'add car out'}
 
